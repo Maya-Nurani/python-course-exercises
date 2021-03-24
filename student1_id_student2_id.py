ef11eb96613dc1af53918d417415f5011ced5545
MayaNurani <maya.nurani@theculturetrip.com>
2021-03-25 00:48:06 +0200

    Adding Rectangle class

--- student1_id_student2_id.py
+++ student1_id_student2_id.py
@@ -16,6 +16,7 @@ class Point:
         self.x += x
         self.y += y
 
+
 p1 = Point(1, 1)
 p2 = Point(2, 2)
 
@@ -24,3 +25,38 @@ p1.addGeneric(x=3, y=2)
 
 print('This is point1', p1)
 print('This is point2', p2)
+
+
+# --------------------------
+class Rectangle:
+    def __init__(self, x, y, width, height):
+        Point.__init__(self, x, y)
+        self.width = width
+        self.height = height
+        self.start_position = Point(x, y)
+
+    def __str__(self):
+        return str(self.width) + "," + str(self.height) + ", " + str(self.start_position)
+
+    def get_area(self):
+
+        return 1
+
+    def get_diag_length(self):
+        diag_point = Point(self.x + self.width, self.y - self.height)
+        # distance =
+
+        return 2   # distance
+
+obj = Rectangle(1,2, 5,6,)
+print('This is Rectangle data:', obj)
+
+
+'''
+    def squared(self):
+        print(math.pow(self.x, 2))
+
+    def addGeneric(self, x=0, y=0):
+        self.x += x
+        self.y += y
+'''
