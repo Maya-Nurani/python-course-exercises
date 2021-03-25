6cfa183207d89e85a732373a76d860bcc2728140
MayaNurani <maya.nurani@theculturetrip.com>
2021-03-25 12:23:55 +0200

    Adding get_area() function and clean code

--- student1_id_student2_id.py
+++ student1_id_student2_id.py
@@ -9,9 +9,7 @@ class Point:
     def __str__(self):
         return "(" + str(self.x) + "," + str(self.y) + ")"
 
-    def squared(self):
-        print(math.pow(self.x, 2))
-
+    # not relevant for the project, this is example for a lesson
     def addGeneric(self, x=0, y=0):
         self.x += x
         self.y += y
@@ -20,7 +18,6 @@ class Point:
 p1 = Point(1, 1)
 p2 = Point(2, 2)
 
-p1.squared()
 p1.addGeneric(x=3, y=2)
 
 print('This is point1', p1)
@@ -45,18 +42,9 @@ class Rectangle:
         diag_point = Point(self.x + self.width, self.y - self.height)
         # distance =
 
-        return 2   # distance
-
-obj = Rectangle(1,2, 5,6,)
-print('This is Rectangle data:', obj)
-print(Rectangle.get_area(obj))
+        return 0  # according to distance between points formula
 
 
-'''
-    def squared(self):
-        print(math.pow(self.x, 2))
-
-    def addGeneric(self, x=0, y=0):
-        self.x += x
-        self.y += y
-'''
+first_rectangle = Rectangle(1, 2, 5, 6, )
+print('This is Rectangle data:', first_rectangle)
+print(Rectangle.get_area(first_rectangle))
