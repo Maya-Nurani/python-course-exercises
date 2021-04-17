4bed2e5a7771c1dfe16f8553f6f7860c3ce0bdb0
MayaNurani <maya.nurani@theculturetrip.com>
2021-04-17 11:44:58 +0300

    Organize code

--- tempExercise.py
+++ tempExercise.py
@@ -1,20 +1,27 @@
 import numpy as np
 from numpy import random
 
+#### PartA exercise1 ####
+initial_temp = random.randint(20, 40)
+print (initial_temp)
+
+
+#### PartA exercise2 ####
+temp_change = random.normal(loc=5, scale=2, size=(14, 24))
+
+
+#### PartA exercise3 ####
+
+#### Draft ####
+
 temp_total = np.ones([14,24])
 print(temp_total)
 print(temp_total.size)
 
-initial_temp = random.randint(20, 40)
-
-temp_change = random.normal(loc=5, scale=2, size=(14, 24))
-
 #np.round(x)
-print (initial_temp)
 #temp_total[0, 0] = initial_temp + temp_change[0,0]
 
 #print(temp_total[0, 0])
 print(temp_change[0,[0]]+initial_temp)
 print(temp_change[0,[1]]+temp_change[0,[0]]+initial_temp)
 #print(temp_change)
-
