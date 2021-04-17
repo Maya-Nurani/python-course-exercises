a9a9b538f76ff1c09b995ed18deb172bd4de2bab
MayaNurani <maya.nurani@theculturetrip.com>
2021-04-17 23:54:40 +0300

    Adding Part B exercise 1-4

--- tempExercise.py
+++ tempExercise.py
@@ -1,40 +1,79 @@
 import numpy as np
 from numpy import random
-
 import matplotlib.pyplot as plt
 
 #### PartA exercise1 ####
 initial_temp = random.randint(20, 41)
-print (initial_temp)
-
+print(initial_temp)
 
 #### PartA exercise2 ####
 temp_change = random.normal(loc=5, scale=2, size=(14, 24))
 print(temp_change)
 
-
 #### PartA exercise3 ####
 temp_total = temp_change
 print(temp_total)
-temp_total[0,0]=temp_total[0,0]+initial_temp
+temp_total[0, 0] = temp_total[0, 0] + initial_temp
 temp_total = np.cumsum(temp_total)
-temp_total = np.reshape(temp_total,(14,24))
+temp_total = np.reshape(temp_total, (14, 24))
+print("print total after sum")
 print(temp_total)
-print(temp_total.shape)
+print("Print shape", temp_total.shape)
 
-#### Part B exercise 4 ####
+
+#### Part B exercise 1 (average) ####
+def average_per_day(temp_total):
+    day_number = 1
+    for i in temp_total:
+        print("The average temperature of day", day_number, " is ", np.average(i))
+        day_number += 1
+
+
+print("average def:")
+average_per_day(temp_total)
+
+
+#### Part B exercise 2 (max) ####
+def max_temp_per_day(temp_change):
+    day_number = 1
+    for i in temp_total:
+        print("The maximum temperature change during day", day_number, " is ", np.max(i))
+        day_number += 1
+
+
+print("max def:")
+max_temp_per_day(temp_change)
+
+
+#### Part B exercise 3 (min) ####
+def min_temp_per_day(temp_change):
+    day_number = 1
+    for i in temp_total:
+        print("The minimum temperature change during day", day_number, " is ", np.min(i))
+        day_number += 1
+
+
+print("min def:")
+min_temp_per_day(temp_change)
+
+
+#### Part B exercise 4 (equal temp per hour) ####
 def check_hourly_temp_equal(temp_change):
-    for i in range(0,10):
-        column = temp_change[ : ,i]
+    for hour in range(0, 24):
+        column = temp_change[:, hour]
+        print(np.round(column))  ### TODO: decide if round and if not - remove this row
         result = np.all(column == column[0])
         if result:
-            print('All Values in column are same / equal')
+            print("Pattern found at hour", hour)
         else:
-            print('All Values in column are not same')
+            print("All temp changes at hour", hour+1, "are not the same")
+
+
+check_hourly_temp_equal(temp_change)
 
 
 ### Part C-1
-def convertTo1d(temp_change,temp_total):
+def convertTo1d(temp_change, temp_total):
     new_temp_change = np.ndarray.flatten(temp_change)
     new_temp_total = np.ndarray.flatten(temp_total)
     return new_temp_change, new_temp_total
@@ -45,27 +84,22 @@ temp_change, temp_total = convertTo1d(temp_change, temp_total)
 
 
 ### Check for C-1
-print (temp_change, temp_total)
+print(temp_change, temp_total)
 print(temp_total.shape)
 
 
 #### Draft ####
 
-temp_total = np.ones([14,24])
-print(temp_total)
-print(temp_total.size)
+# np.round(x)
+# temp_total[0, 0] = initial_temp + temp_change[0,0]
 
-#np.round(x)
-#temp_total[0, 0] = initial_temp + temp_change[0,0]
+# print(temp_total[0, 0])
+# print(temp_change[0,[0]]+initial_temp)
+# print(temp_change[0,[1]]+temp_change[0,[0]]+initial_temp)
+# print(temp_change)
 
-#print(temp_total[0, 0])
-#print(temp_change[0,[0]]+initial_temp)
-#print(temp_change[0,[1]]+temp_change[0,[0]]+initial_temp)
-#print(temp_change)
-
-## for test
+# for test
 arr = np.arange(100)
-arr.shape = (10,10)
-print(arr)
-print(arr[ : ,0])
-check_hourly_temp_equal(arr)
+arr.shape = (10, 10)
+# print(arr)
+# print(arr[ : ,0])
