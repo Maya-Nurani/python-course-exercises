09a6cdc732044e2ddf087487d3ca40ebdf576c95
MayaNurani <maya.nurani@theculturetrip.com>
2021-04-17 12:26:26 +0300

    Adding Part B exercise 4

--- tempExercise.py
+++ tempExercise.py
@@ -8,9 +8,22 @@ print (initial_temp)
 
 #### PartA exercise2 ####
 temp_change = random.normal(loc=5, scale=2, size=(14, 24))
+print(temp_change)
 
 
 #### PartA exercise3 ####
+
+#### Part B exercise 4 ####
+def check_hourly_temp_equal(temp_change):
+    for i in range(0,10):
+        column = temp_change[ : ,i]
+        result = np.all(column == column[0])
+        if result:
+            print('All Values in column are same / equal')
+        else:
+            print('All Values in column are not same')
+
+
 
 #### Draft ####
 
@@ -25,3 +38,10 @@ print(temp_total.size)
 print(temp_change[0,[0]]+initial_temp)
 print(temp_change[0,[1]]+temp_change[0,[0]]+initial_temp)
 #print(temp_change)
+
+## for test
+arr = np.arange(100)
+arr.shape = (10,10)
+print(arr)
+print(arr[ : ,0])
+check_hourly_temp_equal(arr)
