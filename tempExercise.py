ad73431fd8208715e2c359c29ef1d13c0dd9266b
Laly <cptlaly@gmail.com>
2021-04-17 18:47:06 +0300

    added a function to convert arrays to 1d (question c-1)

--- tempExercise.py
+++ tempExercise.py
@@ -16,7 +16,9 @@ temp_total = temp_change
 print(temp_total)
 temp_total[0,0]=temp_total[0,0]+initial_temp
 temp_total = np.cumsum(temp_total)
+temp_total = np.reshape(temp_total,(14,24))
 print(temp_total)
+print(temp_total.shape)
 
 #### Part B exercise 4 ####
 def check_hourly_temp_equal(temp_change):
@@ -27,6 +29,22 @@ def check_hourly_temp_equal(temp_change):
             print('All Values in column are same / equal')
         else:
             print('All Values in column are not same')
+
+
+### Part C-1
+def convertTo1d(temp_change,temp_total):
+    new_temp_change = np.ndarray.flatten(temp_change)
+    new_temp_total = np.ndarray.flatten(temp_total)
+    return new_temp_change, new_temp_total
+
+
+### convertTo1d run
+temp_change, temp_total = convertTo1d(temp_change, temp_total)
+
+
+### Check for C-1
+print (temp_change, temp_total)
+print(temp_total.shape)
 
 
 
@@ -40,8 +58,8 @@ print(temp_total.size)
 #temp_total[0, 0] = initial_temp + temp_change[0,0]
 
 #print(temp_total[0, 0])
-print(temp_change[0,[0]]+initial_temp)
-print(temp_change[0,[1]]+temp_change[0,[0]]+initial_temp)
+#print(temp_change[0,[0]]+initial_temp)
+#print(temp_change[0,[1]]+temp_change[0,[0]]+initial_temp)
 #print(temp_change)
 
 ## for test
