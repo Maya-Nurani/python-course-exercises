1c88a66c2f72edb8c2976d5fdd3dfa8430ad4318
Laly <cptlaly@gmail.com>
2021-05-02 19:13:45 +0300

    created dataframe of x,y and z

--- student1_id_student2_id.py
+++ student1_id_student2_id.py
@@ -12,11 +12,16 @@ def z(y):
 
 
 #x_arr = random.normal(loc=0, scale=2, size=(10000))
-x_arr = [1,2,3,4,5]
+#x array for an easy check of y and z
+x_arr = [-2,0,2,3,4,5]
 y_arr = np.array([y(xi) for xi in x_arr])
 z_arr = np.array([z(yi) for yi in y_arr])
+dataset = pd.DataFrame({'x': x_arr, 'y': y_arr, 'z':z_arr})
 
+#Check for dataset (DELETE AT FINISH)
+print (dataset)
+
+#Check for x,y and z arrays (DELETE AT FINISH)
 print (x_arr)
 print (y_arr)
-print (x_arr)
 print (z_arr)
