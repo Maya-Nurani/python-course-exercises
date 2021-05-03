8fbc31ee23ca97e1c0c5df0a4d0d48e3a06ea2de
Laly <cptlaly@gmail.com>
2021-05-03 20:05:49 +0300

    FINISHED EXERCISE A + CHECKED EVERYTHING

--- student1_id_student2_id.py
+++ student1_id_student2_id.py
@@ -4,6 +4,7 @@ from numpy import random
 import matplotlib.pyplot as plt
 
 
+# Part A Dataframe creation
 def y(x):
     return np.power(x, 3) - 2 * np.power(x, 2);
 
@@ -12,20 +13,13 @@ def z(y):
     return np.sqrt(np.power(np.e, y))
 
 
-# x_arr = random.normal(loc=0, scale=2, size=(10000))
-# x array for an easy check of y and z
-x_arr = [-2, 0, 2, 3, 4, 5]
+x_arr = random.normal(loc=0, scale=2, size=(10000))
 y_arr = np.array([y(xi) for xi in x_arr])
 z_arr = np.array([z(yi) for yi in y_arr])
 dataset = pd.DataFrame({'x': x_arr, 'y': y_arr, 'z': z_arr})
 
 # Check for dataset (DELETE AT FINISH)
 print(dataset)
-
-# Check for x,y and z arrays (DELETE AT FINISH)
-print(x_arr)
-print(y_arr)
-print(z_arr)
 
 # Part A question 1
 print("The maximum value of y is: {0}".format(dataset['y'].max()))
@@ -35,21 +29,38 @@ print("The minimum value of z is: {0}".format(dataset['z'].min()))
 
 
 # Part A question 3
-def findDuplicates():
+def find_duplicates():
     return dataset[dataset['x'] == dataset['y']]
 
 
-print("The duplicates between Y and Z are: \n {0}".format(findDuplicates()))
+print("The duplicates between Y and Z are: \n {0}".format(find_duplicates()))
 
 # Part A question 4
 print("The first 5 rows of X and Z columns of the dataset: \n {0}".format(dataset[['x', 'z']].head()))
 
 # Part A question 5
-# def new_column_creation(x,y):
-#   return 2*x-np.power(x,2)
-# new_column = np.array([new_column_creation(i) for i in x_arr])
+dataset['newColumn'] = new_column = 2*dataset['x'] - np.power(dataset['y'], 2)
 
-#Part A question 6
+
+# Part A questions 6, 7, 8
+def show_scatter(a,b):
+    plt.scatter(dataset[a], dataset[b], color='purple')
+    plt.xlabel(str(a) + ' Axis')
+    plt.ylabel(str(b) + ' Axis')
+    plt.grid(color='pink')
+    plt.show()
+
+show_scatter('x', 'y')
+show_scatter('x', 'z')
+
+print(dataset)
+
+# Part A question 9
+plt.scatter(np.arange(3500), dataset.sample(frac=0.35)['newColumn'], color='black')
+plt.xlabel('Index')
+plt.ylabel('2x-y^2')
+plt.grid(color='green')
+plt.show()
 
 # Part B question 1
 flights_data_df = pd.read_csv('flights.csv')
