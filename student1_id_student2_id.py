843b6deb7693fbf7200781dc45c92f202c3d0bae
MayaNurani <maya.nurani@theculturetrip.com>
2021-05-06 14:44:32 +0300

    Last clean before submission

--- student1_id_student2_id.py
+++ student1_id_student2_id.py
@@ -6,20 +6,17 @@ import matplotlib.pyplot as plt
 
 # Part A Dataframe creation
 def y(x):
-    return np.power(x, 3) - 2 * np.power(x, 2);
+    return np.power(x, 3) - 2 * np.power(x, 2)
 
 
 def z(y):
     return np.sqrt(np.power(np.e, y))
 
 
-x_arr = random.normal(loc=0, scale=2, size=(10000))
+x_arr = random.normal(loc=0, scale=2, size=10000)
 y_arr = np.array([y(xi) for xi in x_arr])
 z_arr = np.array([z(yi) for yi in y_arr])
 dataset = pd.DataFrame({'x': x_arr, 'y': y_arr, 'z': z_arr})
-
-# Check for dataset (DELETE AT FINISH)
-print(dataset)
 
 # Part A question 1
 print("The maximum value of y is: {0}".format(dataset['y'].max()))
@@ -30,16 +27,16 @@ print("The minimum value of z is: {0}".format(dataset['z'].min()))
 
 # Part A question 3
 def find_duplicates():
-    return dataset[dataset['x'] == dataset['y']]
+    return dataset[dataset['y'] == dataset['z']]
 
 
-print("The duplicates between Y and Z are: \n {0}".format(find_duplicates()))
+print("The values with duplication between Y and Z are: \n {0}".format(find_duplicates()))
 
 # Part A question 4
 print("The first 5 rows of X and Z columns of the dataset: \n {0}".format(dataset[['x', 'z']].head()))
 
 # Part A question 5
-dataset['newColumn'] = new_column = 2*dataset['x'] - np.power(dataset['y'], 2)
+dataset['newColumn'] = 2 * dataset['x'] - np.power(dataset['y'], 2)
 
 
 # Part A questions 6, 7, 8
@@ -54,7 +51,6 @@ def show_scatter(a, b):
 show_scatter('x', 'y')
 show_scatter('x', 'z')
 
-
 # Part A question 9
 plt.scatter(np.arange(3500), dataset.sample(frac=0.35)['newColumn'], color='black')
 plt.xlabel('Index')
@@ -63,14 +59,16 @@ plt.grid(color='green')
 plt.title('2x-y^2 column graph')
 plt.show()
 
+# Part B
+
 # Part B question 1
 flights_data_df = pd.read_csv('flights.csv')
 
 # Part B question 2
-# number of rows
+# Number of rows
 rows = len(flights_data_df.axes[0])
 print("Number of rows = ", rows)
-# computing number of columns
+# Computing number of columns
 cols = len(flights_data_df.axes[1])
 print("Number of columns = ", cols)
 
@@ -81,52 +79,59 @@ print("Print columns that contain NaN values", flights_data_df.columns[flights_d
 # Part B question 4
 print("The most frequent day is:", flights_data_df['Day'].value_counts().idxmax())
 
-# Part B - queries - 1
+# Part B - Queries
+# Part B - query 1
 print("The state in USA that have the most number of flights is ", flights_data_df['Carrier'].value_counts().idxmax())
 
-# Part B - queries - 2
+# Part B - query 2
 print("The number of flights to EWR,New-york destination is",
       flights_data_df.groupby('Dest').count()['flightId']['EWR'])
 
-# Part B - queries - 3
+# Part B - query 3
 print("The number of flights that departure from DCA and arrived to JFK is",
       flights_data_df[(flights_data_df['Dest'] == 'JFK') & (flights_data_df['Origin'] == 'DCA')].count()['flightId'])
 
-# Part B - queries - 4
+# Part B - query 4
 num_delayed_flights = flights_data_df[flights_data_df['Delayed'] == 1].count()['flightId']
-print("The percentage of delayed flights out of all flights is", (num_delayed_flights/rows)*100, "%")
+print("The percentage of delayed flights out of all flights is", (num_delayed_flights / rows) * 100, "%")
 
-# Part B - queries - 5
-num_delayed_flights_and_rainy = flights_data_df[(flights_data_df['Delayed'] == 1) & (flights_data_df['Weather'] == 2)].count()['flightId']
-print("The percentage of delayed and cloudy flights out of all flights is", (num_delayed_flights_and_rainy/rows)*100, "%")
+# Part B - query 5
+num_delayed_flights_and_rainy = \
+    flights_data_df[(flights_data_df['Delayed'] == 1) & (flights_data_df['Weather'] == 2)].count()['flightId']
+print("The percentage of delayed and cloudy flights out of all flights is",
+      (num_delayed_flights_and_rainy / rows) * 100, "%")
 
-# Part B - queries - 6
+# Part B - query 6
 unique_distances = flights_data_df[flights_data_df['Carrier'] == 'DH']['Dest'].unique()
 print("The unique different destinations for DH Carrier are:", unique_distances)
 
-# dt column
+# keeping 'dt' column as a variable
 dt = flights_data_df['DepTime']
 
-# Part B - queries - 7
+# Part B - query  7
 num_morning_flights = flights_data_df[(dt >= 1000) & (dt <= 1500)].count()['flightId']
 print("The number of flights that departure between 10:00 to 15:00 is", num_morning_flights)
 
-# Part B - queries - 8
-num_WED_noon_flights = flights_data_df[(dt >= 1400) & (dt <= 1700) & (flights_data_df['Day'] == 'WED')].count()['flightId']
-print("The number of flights that departure between 14:00 to 17:00 and day = Wednesday is", num_WED_noon_flights)
+# Part B - query 8
+num_WED_noon_flights = flights_data_df[(dt >= 1400) & (dt <= 1700) & (flights_data_df['Day'] == 'WED')].count()[
+    'flightId']
+print("The number of flights that departure between 14:00 to 17:00 on Wednesday is", num_WED_noon_flights)
 
-# Part B - queries - 9
-num_WED_noon_JFK_flights = flights_data_df[(dt >= 1300) & (dt <= 1700) & (flights_data_df['Day'] == 'WED') & (flights_data_df['Dest'] == 'JFK')].count()['flightId']
-print("The number of flights that departure between 14:00 to 17:00 on Wednesday and arrived to JFK is", num_WED_noon_JFK_flights)
+# Part B - query 9
+num_WED_noon_JFK_flights = flights_data_df[
+    (dt >= 1300) & (dt <= 1700) & (flights_data_df['Day'] == 'WED') & (flights_data_df['Dest'] == 'JFK')].count()[
+    'flightId']
+print("The number of flights that departure between 13:00 to 17:00 on Wednesday and arrived to JFK is",
+      num_WED_noon_JFK_flights)
 
-# Part B - queries - 10
+# Part B - query 10
 latest_time = dt.max()
-print("The latest time at the system is", latest_time)
+print("The latest departure time at the system is", latest_time)
+
 
 # Part B - Visualizations
-
 # Visualization 1: a pie chart that shows the part of the delayed flights in the total amount (query 4)
-pieSlices = (num_delayed_flights, rows-num_delayed_flights)
+pieSlices = (num_delayed_flights, rows - num_delayed_flights)
 pieTitles = ('Delayed Flights', 'Flights on time')
 pieColors = ('r', 'y')
 plt.title('Delayed flights percent')
@@ -136,13 +141,14 @@ plt.show()
 # Visualization 2: a pie chart that show to percent of JFK flights in all afternoon Wednesday flights (query 9)
 wedPieSlices = (num_WED_noon_JFK_flights, num_WED_noon_flights)
 wedPieTitles = ('JFK WED FLIGHTS', 'OTHER WED FLIGHTS')
-wedPieColors = ('b','g')
+wedPieColors = ('b', 'g')
 plt.title('JFK Wednesday afternoon flights percent')
 plt.pie(wedPieSlices, labels=wedPieTitles, colors=wedPieColors, autopct='%1.1f%%')
 plt.show()
 
 # Visualization 3: A graph that shows the amount of flights for each carrier (query 1)
-plt.plot(flights_data_df.groupby('Carrier').count()['flightId'], marker='o', color='g', markeredgecolor='g', linestyle='none')
+plt.plot(flights_data_df.groupby('Carrier').count()['flightId'], marker='o', color='g', markeredgecolor='g',
+         linestyle='none')
 plt.title('Amount of flights for each carrier')
 plt.grid()
 plt.show()
