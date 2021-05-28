85eebc3fb9145c9b179bc03b0954141b4c73923f
MayaNurani <maya.nurani@theculturetrip.com>
2021-05-28 12:32:44 +0300

    # Part B - Query 3 and 5 And continue part C

--- AdvancedPandas.py
+++ AdvancedPandas.py
@@ -20,7 +20,8 @@ plt.title("Group value amount")
 plt.show()
 
 # Part A ex. 3
-print('the amount of studends is: {0}'.format(students_data_df.groupby('parental level of education').count()['gender']['high school']))
+print('the amount of studends is: {0}'.format(
+    students_data_df.groupby('parental level of education').count()['gender']['high school']))
 
 
 # Part B - Query 1
@@ -46,9 +47,19 @@ completed_stud = students_data_df[students_data_df['test preparation course'] ==
 group = completed_stud['race/ethnicity'].value_counts().head(1)
 print("The group with the most students that finish pre-course:", group.index.values.tolist()[0])
 
-
-
 # Part B - Query 3
+average_group = students_data_df.groupby('race/ethnicity').mean()['math score']
+print(average_group)
+average_group.plot(kind="bar", color='b')
+plt.grid()
+plt.ylabel('Average number')
+plt.title("Average math score per each group")
+plt.show()
+
+
+# Part B - Query 5
+average_gender = students_data_df.groupby('gender').mean()
+print(average_gender)
 
 
 # Part C Q1
@@ -64,9 +75,33 @@ lst1 = [2, 4, 6]
 lst2 = [1, 2, 3]
 print(distance(lst1, lst2))
 
-# Part C question 2  > not completed
-items = ['A','B','C','D','E']
-df = pd.DataFrame( columns = ['id', 'item'], index=range(10))
-df['id'] = range(10)
-df = df['item'].apply(lambda x: random.choice(items))
-print(df)
+# Part C question 2
+items = ['A', 'B', 'C', 'D', 'E']
+df = pd.DataFrame(columns=['id', 'item'])
+df['id'] = random.sample(range(0, 100), 100)
+df['item'] = df['item'].apply(lambda x: random.choice(items))
+
+print(df.head())
+
+item_count = df['item'].value_counts()
+print(item_count)
+print("Do we have all the groups in the data frame? ", set(item_count.index.values) == set(items))
+
+# Part C question 3
+groups_df = pd.get_dummies(df, columns=["item"])
+print(groups_df.head())  # TODO: check if need to remove the print
+
+# Part C question 4 - adding new column fo distance
+groups_df["distance"] = groups_df.values.tolist()
+groups_df["distance"] = groups_df["distance"].apply(lambda row: distance([0,1,0,0,0],row[1:6]))
+
+print(groups_df.head())
+
+# Part C question 5
+groups_df.groupby("distance")
+
+
+groups_df.groupby("new column").min()
+# Part C question 6
+
+
