3810c1dc400ca0c56d44389f4fe5cdd67a491bbe
Laly <cptlaly@gmail.com>
2021-05-29 19:45:52 +0300

    removed prints, fixed spacing and other little things finished b-4, started b-5

--- AdvancedPandas.py
+++ AdvancedPandas.py
@@ -10,7 +10,7 @@ except:
     print("Failed to read the file")
     students_data_df = []  # In case the file is not read
 
- # Part A ex. 1
+# Part A ex. 1
 print("There are", students_data_df.shape[0], "rows and", students_data_df.shape[1], "columns in this file.")
 print("Columns names are: ", list(students_data_df.columns))
 print(students_data_df.describe())
@@ -23,8 +23,7 @@ plt.show()
 
 # Part A ex. 3
 total_students = int(len(students_data_df.axes[0]))
-print(total_students)  # TODO: need to remove or not?
-print('the percent of highschol studends is: {0}'.format(
+print('the percent of highschool students is: {0}'.format(
     students_data_df.groupby('parental level of education').count()['gender']['high school'] / total_students))
 
 # Part A ex. 4
@@ -44,11 +43,13 @@ print('Males amount: {0}, Females amount: {1}'.format(students_data_df['gender_m
 # Part A ex. 7
 students_data_df = students_data_df.rename(columns={"race/ethnicity": "ethnicity"})
 
+
 # Part A ex. 8
 # a function that returns the last chart of a string
 def get_last_char(string):
     string = string[-1]
     return string
+
 
 # Part A ex. 9
 students_data_df['ethnicity'] = students_data_df['ethnicity'].apply(lambda x: get_last_char(x))
@@ -57,7 +58,7 @@ students_data_df['ethnicity'] = students_data_df['ethnicity'].apply(lambda x: ge
 students_data_df["test preparation course"] = students_data_df["test preparation course"].replace \
     ({'completed': 1, 'none': 0})
 
-# Part A ex. 11 makes 'lunch' binaric - standart = 0, free/reduced = 1
+# Part A ex. 11 makes 'lunch' binaric: standart = 0, free/reduced = 1
 students_data_df["lunch"] = pd.get_dummies(students_data_df['lunch'], prefix='lunch')
 
 # Part A ex. 12
@@ -73,42 +74,47 @@ ethnicity_dict = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5}
 students_data_df["ethnicity"] = students_data_df["ethnicity"].map(ethnicity_dict)
 
 
-# Part B - Query 1
+# Part B ex. 1
 def reduce_average_and_std_from_score(columns):
     for col in columns:
         m = students_data_df[col].mean()
         s = students_data_df[col].std()
-        print('m=', m, "s=", s)
-        # students_data_df[col] = students_data_df[col].apply(lambda grade: (grade - m) / s)   #Another way of calculation
         students_data_df[col] = (students_data_df[col] - m) / s
 
 
 column_names = ["math score", "reading score", "writing score"]
 reduce_average_and_std_from_score(column_names)
 
-# TODO: remove this print before submission
-print(students_data_df[column_names].head())
-
-# Part B - Query 2
+# Part B ex. 2
 completed_stud = students_data_df[students_data_df['test preparation course'] == 1]
 group = completed_stud['ethnicity'].value_counts().head(1)
 print("The group with the most students that finish pre-course:", group.index.values.tolist()[0])
 
-# Part B - Query 3
+# Part B ex. 3
 average_group = students_data_df.groupby('ethnicity').mean()['math score']
-print(average_group) # TODO: remove print because having diagram
 average_group.plot(kind="bar", color='b')
 plt.grid()
 plt.ylabel('Average number')
 plt.title("Average math score per each group")
 plt.show()
 
-# Part B - Query 5
-average_gender = students_data_df.groupby(['gender_male', 'gender_female']).mean()
-print(average_gender)  # TODO: need to finish, create 2 Pie, one for women one for men
+# Part B ex. 4
+new_df = students_data_df[["math score", "reading score", "writing score"]].copy()
+new_df.hist()
+plt.show()
+# the graph shows us the distribution of the grades at each subject (grade/amount of students that have it)
 
+# Part B ex. 5
+df_female_grades = students_data_df.where(students_data_df['gender_female']==1)[["math score", "reading score", "writing score"]]
+df_female_grades = df_female_grades.dropna() #removing NaN values (male values)
+df_male_grades = students_data_df.where(students_data_df['gender_female']==0)[["math score", "reading score", "writing score"]]
+df_male_grades = df_male_grades.dropna() #removing NaN values (female values)
+print(df_female_grades)
+print(df_male_grades)
+# TODO: need to finish, pie graph cannot have negative values
 
-# Part C Q1
+# Part C ex. 1
+
 
 def distance(lst1, lst2):
     arr1 = np.array(lst1)
@@ -116,12 +122,7 @@ def distance(lst1, lst2):
     dist = np.linalg.norm(arr1 - arr2)
     return dist
 
-# TODO: check if need to remove lists and print at the end
-lst1 = [2, 4, 6]
-lst2 = [1, 2, 3]
-print(distance(lst1, lst2))
-
-# Part C question 2
+# Part C ex. 2
 items = ['A', 'B', 'C', 'D', 'E']
 df = pd.DataFrame(columns=['id', 'item'])
 df['id'] = random.sample(range(0, 100), 100)
@@ -133,21 +134,21 @@ item_count = df['item'].value_counts()
 print(item_count)
 print("Do we have all the groups in the data frame? ", set(item_count.index.values) == set(items))
 
-# Part C question 3
+# Part C ex. 3
 groups_df = pd.get_dummies(df, columns=["item"])
-print(groups_df.head())  # TODO: check if need to remove the print
 
-# Part C question 4 - adding new column fo distance
+# Part C ex. 4 - adding new column fo distance
 groups_df["distance"] = groups_df.values.tolist()
 groups_df["distance"] = groups_df["distance"].apply(lambda row: distance([0, 1, 0, 0, 0], row[1:6]))
 
 print(groups_df.head())
 
-# Part C question 5
+# Part C ex. 5
 group_by_distance = groups_df.groupby('distance')
 # Those rows values are the same like this list [0, 1, 0, 0, 0]
 print(group_by_distance.get_group(0))
 
-# Part C question 6
+# Part C ex. 6
+#TODO: maya - explain why this is the result (appears at the question)
 if (groups_df["distance"].max() != 0):
     print(groups_df["distance"].max())
